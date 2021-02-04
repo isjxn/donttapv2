dfc8f8666c2f16050844091aa76522e81720e76d
isjxn <jani123123123.diederich@gmx.de>
2021-02-04 22:14:25 +0100

    added hits and misses

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,9 @@ window.onload = () => {
     var activeSquares = [];
     var clicked;
     var closest;
+    var misses = 0;
+    var hits = 0;
+    var accuracy = 0;
 
     const getRandomNumber = () => {
         return Math.floor(Math.random() * 16) + 1;
@@ -32,6 +35,13 @@ window.onload = () => {
         closest = document.elementFromPoint(event.pageX, event.pageY);
     }
 
+    const updateScores = () => {
+        document.getElementById('hitScore').innerHTML = hits;
+        document.getElementById('missScore').innerHTML = misses;
+        //accuracy = hits * 100 / misses;
+        //document.getElementById('accuracy').innerHTML = accuracy;
+    }
+
     const handleClick = () => {
         const children = gameField.children;
         for (let child of children) {
@@ -45,10 +55,13 @@ window.onload = () => {
                 clicked = Number(id);
                 removeItemOnce(Number(id));
                 createActiveSquares();
+                hits += 1;
             } else {
                 closest.classList.add('miss');
+                misses += 1;
             }
         }
+        updateScores();
     }
 
     createActiveSquares();
